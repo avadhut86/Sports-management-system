78a42af73d4ab36b145fda866c21a3a3b90b38d9
avadhut86 <avipitale2003@gmail.com>
2024-07-14 19:57:41 +0800

    start todo

--- Todo list/src/App.jsx
+++ Todo list/src/App.jsx
@@ -1,34 +1,49 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [name,setName] = useState("")
+  const [num,setNum] = useState("123")
+
+  const [todos,setTodos] = useState([])
+
+  function myfunc(){
+    setName("Hello")
+  }
+
+  function myfunc1(){
+    setNum("456")
+  }
+
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div>
+    <h1>TODO LIST {name}</h1>
+    <form onSubmit={(e) =>{
+     e.preventDefault();
+     setTodos([
+      {title: name}
+     ])
+     console.log(todos)
+    }}>
+    <input type="text" placeholder='Enter here' value={name} 
+    onChange={(e) => 
+      setName(e.target.value)}
+    /> <br />
+    <button type='submit'>Add Todo</button>
+    </form>
+    <p>Todo - List {num}</p>
+    <input type="checkbox" />
+   <h1> {todos.length}</h1>
+   {todos.length > 0 && <ul>
+      <li className='task'><label htmlFor=""><input type="checkbox" />{todos[0].title}</label><button className='btn' onClick={myfunc1}>Delete</button></li>
+      {/* <li className='task'><label htmlFor=""><input type="checkbox"/>{todos[1]}</label><button className='btn'>Delete</button></li> */}
+    </ul>}
+    
+    
+   </div>
   )
 }
 
